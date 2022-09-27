95051b11c3e049e9f940ecf05d1ade78aa2ba2e3
hiroshiimaizumi <hiroshiimaizumi0611@gmail.com>
2022-09-27 17:49:19 +0900

    Add data access for remaining microservices

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddDbContext<PrecipitationDbContext>(options => {
     options.EnableSensitiveDataLogging();
     options.EnableDetailedErrors();
--- CloudWeather.Report/Program.cs
+++ CloudWeather.Report/Program.cs
@@ -1,4 +1,14 @@
+using CloudWeather.Report.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<WeatherReportDbContext>(options => {
+    options.EnableSensitiveDataLogging();
+    options.EnableDetailedErrors();
+    options.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+}, ServiceLifetime.Transient
+);
 
 var app = builder.Build();
 
