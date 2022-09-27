074b31964dd08fe4352b2054fb41ec9d3367b757
hiroshiimaizumi <hiroshiimaizumi0611@gmail.com>
2022-09-27 18:26:01 +0900

    Add endpoints for services

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -28,4 +28,11 @@ app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, Preci
     return Results.Ok(results);
 });
 
+app.MapPost("/observation/", async (Precipitation precipitation, PrecipitationDbContext db) =>
+{
+    precipitation.CreatedOn = precipitation.CreatedOn.ToUniversalTime();
+    await db.AddAsync(precipitation);
+    await db.SaveChangesAsync();
+});
+
 app.Run();
